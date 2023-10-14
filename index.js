88389506cb58eb9b2bd30c1b4775d2443f4126d2
Guilherme Casagranda <gmcasagranda@gmail.com>
2023-10-14 16:15:01 -0300

    parâmetros de login na rota

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const url = require('url');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get('/', function(req, res){
     res.sendFile(path.join(__dirname + '/html/login.html'));
+});
+
+app.post('/confirma', function(req, res){
+    res.send('Login efetuado com sucesso: ' + req.body.user);
+});
+
+app.get('/confirma', function(req, res){
+    q = url.parse(req.url, true);
+    res.send('Login efetuado com sucesso: ' + q.query.user);
 });
 
 app.get('/cadastro', function(req, res){
